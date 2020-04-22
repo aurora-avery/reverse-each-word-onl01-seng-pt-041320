714b017275929d43376a436f85c602b1d37a0930
Aurora Avery <aurora.avery@yahoo.com>
2020-04-22 04:50:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,9 @@
 def reverse_each_word(sentence)
- sentence.reverse.split.reverse.join("string sentence")
-end 
-def reverse_each_word(sentence)
- sentence.reverse.split.reverse.join(" ")
- end 
+  sentence.split.collect {|word| word.reverse}.join(" ")
+end
+
+# Without iterating:
+
+# def reverse_each_word(sentence)
+#  sentence.reverse.split.reverse.join(" ")
+# end
